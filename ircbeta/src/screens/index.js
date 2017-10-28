4421128bc057ee53736ae1acf3721d6f87f02507
Jeane Carlos <volbohel@gmail.com>
2017-10-28 06:47:45 -0400

    JOHN CENA

--- ircbeta/src/screens/index.js
+++ ircbeta/src/screens/index.js
@@ -3,7 +3,6 @@ export CustomsScreen from './CustomsScreen';
 export EmergencyContact from './EmergencyContactScreen';
 export HealthScreen from './HealthScreen';
 export HomeScreen from './HomeScreen';
-export HealthScreen from './HealthScreen';
 export PlaygroundScreen from './PlaygroundScreen';
 export LocationScreen from './LocationScreen';
 export ContactScreen from './ContactScreen';
