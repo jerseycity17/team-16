9efbe810ea94e87baa15a8f33a57bf113c32869b
justint <justintcoding@gmail.com>
2017-10-28 08:13:46 -0400

    Add a submit to twilio

--- admin/app.js
+++ admin/app.js
@@ -23,7 +23,7 @@ app.use("/", express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-    
+
     auth.onAuthStateChanged((user) => {
         if(user)
         {
@@ -36,7 +36,7 @@ app.get('/', (req, res) => {
 
     });
     //res.sendFile(__dirname +'/public/home.html');
-    
+
 });
 
 
@@ -60,19 +60,19 @@ query.once("value")
 });*/
 app.get('/alerts', async (req, res) => {
     var alertsDatasnapshot = await database.ref("Syria/alerts").orderByKey().once("value");
-    alerts = []; 
-    
+    alerts = [];
+
     alertsDatasnapshot.forEach(function(childSnapshot) {
         alerts.push(childSnapshot.val());
     });
     res.json(alerts);
-    
+
 })
 app.post('/register', async function(req, res) {
-    
+
     var newUserEmail = req.body.email;
     var newUserPass = req.body.pass;
-    
+
     try {
         await auth.createUserWithEmailAndPassword(newUserEmail, newUserPass);
         res.sendFile(__dirname +'/public/login.html');
@@ -86,7 +86,7 @@ app.get('/register', function (req, res){
     console.log("inside get")
     res.sendFile(__dirname +'/public/register.html');
 });
-   
+
 app.post('/logout', async function(req, res) {
     try {
     await auth.signOut();
@@ -102,10 +102,10 @@ app.get('/login', function (req, res){
 });
 
 app.post('/login', async function(req, res){
-    
+
     var userEmail = req.body.email;
     var userPassword = req.body.pass;
-    
+
     try {
         await auth.signInWithEmailAndPassword(userEmail, userPassword);
         res.sendFile(__dirname +'/public/home.html');
@@ -119,4 +119,43 @@ app.get('*', (req, res) => {
 app.listen(3000, () => {
     console.log("We've now got a server!");
     console.log("Your routes will be running on http://localhost:3000");
+});
+
+const twilio = require('twilio');
+const client = new twilio("ACabbc2b33b44f99aa5dbd3602099bd85e", "2bac59072967bb0e26844727613243db");
+const twilioNumber = '+13478518941';
+
+let firstTime = true;
+
+database
+.ref('/Syria/alerts/')
+.on('value', snapshot => {
+    if (firstTime) {
+        firstTime = false;
+        return;
+    }
+    console.log("New create of data");
+     const alertList = snapshot.val();
+     console.log(alertList)
+     const alertListKey = Object.keys(alertList);
+     const most_recent_alert = alertList[alertListKey[alertListKey.length - 1]];
+     const title       = most_recent_alert.title
+     const description = most_recent_alert.description
+     const textMessage = {
+         body: `Title: ${title}\nDescription: ${description}`,
+         to: null,  // Text to this number
+         from: twilioNumber // From a valid Twilio number
+     };
+     database
+         .ref(`/Syria/staff/`)
+         .once('value')
+         .then((snapshot) => {
+             // Send SMS to each person
+             const staffList = snapshot.val();
+             Object.keys(staffList).map(staffId => {
+                 textMessage.to = staffList[staffId].phone_number;
+                 return client.messages.create(textMessage);
+             });
+         })
+     .catch(err => console.log(err));
 });
