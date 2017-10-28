a027661a6c7a96db5dcc9703ffcdb56b8e5ddf04
Matt <mattcolozzo@gmail.com>
2017-10-28 05:51:18 -0400

    a bunch of admin functionality

--- admin/app.js
+++ admin/app.js
@@ -10,6 +10,7 @@ var config = firebase.initializeApp({
     messagingSenderId: "863184103451"
 });
 const database = firebase.database();
+const auth = firebase.auth();
 const app = express();
 
 app.use(bodyParser.urlencoded({
@@ -22,19 +23,90 @@ app.use("/", express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname +'/public/home.html');
+    
+    auth.onAuthStateChanged((user) => {
+        if(user)
+        {
+            res.sendFile(__dirname +'/public/home.html');
+        }
+        else
+        {
+            res.sendFile(__dirname +'/public/login.html');
+        }
+
+    });
+    //res.sendFile(__dirname +'/public/home.html');
+    
 });
 
-app.get('*', (req, res) => {
-    res.sendStatus(404);
-});
+
 
 app.post('/send', async (req, res) => {
    alert = req.body;
    await database.ref('Syria/alerts/').push(alert);
-   console.log(alert)
+});
+/*database.ref('/Syria/emergency').once('value').then((emergency_contact) => {
+            emergency_contact = emergency_contact.val();
+            let emergency = [];
+            Object.keys(emergency_contact).forEach((contact_field) => {
+                let contact_list = Object.keys(emergency_contact[contact_field]).map((contact_item) => {
+                    return contact_item
+                });
+            });*/
+app.get('/alerts', async (req, res) => {
+    var alerts = await database.ref("alerts").once("value");
+    console.log(alerts);
+    res.json(alerts);
+    
+})
+app.post('/register', async function(req, res) {
+    
+    var newUserEmail = req.body.email;
+    var newUserPass = req.body.pass;
+    
+    try {
+        await auth.createUserWithEmailAndPassword(newUserEmail, newUserPass);
+        res.sendFile(__dirname +'/public/login.html');
+    } catch (e) {
+        console.log(e)
+        res.status(500).json(e);
+    }
 });
 
+app.get('/register', function (req, res){
+    console.log("inside get")
+    res.sendFile(__dirname +'/public/register.html');
+});
+   
+app.post('/logout', async function(req, res) {
+    try {
+    await auth.signOut();
+    } catch (e) {
+        res.status(500).json('Error on login');
+    }
+    res.sendFile(__dirname + '/public/login.html');
+});
+
+//Perform authentication
+app.get('/login', function (req, res){
+    res.sendFile(__dirname + '/public/login.html');
+});
+
+app.post('/login', async function(req, res){
+    
+    var userEmail = req.body.email;
+    var userPassword = req.body.pass;
+    
+    try {
+        await auth.signInWithEmailAndPassword(userEmail, userPassword);
+        res.sendFile(__dirname +'/public/home.html');
+    } catch (e) {
+        res.status(500).json(e);
+    }
+});
+app.get('*', (req, res) => {
+    res.sendStatus(404);
+});
 app.listen(3000, () => {
     console.log("We've now got a server!");
     console.log("Your routes will be running on http://localhost:3000");
