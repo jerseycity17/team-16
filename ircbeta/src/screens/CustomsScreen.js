3f844f4fa309b52041d42ca7608722446b219616
Alex Supkay <asupkay1124@gmail.com>
2017-10-27 22:42:42 -0400

    Put panel into customscreen

--- ircbeta/src/screens/CustomsScreen.js
+++ ircbeta/src/screens/CustomsScreen.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { View, Text, StyleSheet } from 'react-native';
 import { connect} from 'react-redux'
+import Panel from './components/Panel';
 
 class CustomsScreen extends Component {
     componentWillMount() {
@@ -13,8 +14,31 @@ class CustomsScreen extends Component {
 
     render() {
         return {
-            <View>
-            </View>
+            <ScrollView style={styles.container}>
+                <Panel title="Social Interaction">
+                    <Text>Dummy text</text>
+                </Panel>
+                <Panel title="Greetings">
+                    <Text>Dummy text</text>
+                </Panel>
+                <Panel title="Men">
+                    <Text>Dummy text</text>
+                </Panel>
+                <Panel title="Woman">
+                    <Text>Dummy text</text>
+                </Panel>
+                <Panel title="Food">
+                    <Text>Dummy text</text>
+                </Panel>
+            </ScrollView>
         }
     }
 }
+
+var styles = StyleSheet.create({
+    container: {
+        flex : 1,
+        backgroundColor : '#f4f7f9',
+        paddingTop : 30
+    }
+});
