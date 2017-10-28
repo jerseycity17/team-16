7725a1017257d5328076310c3cf83d84b5e1bd6c
justint <justintcoding@gmail.com>
2017-10-28 02:57:38 -0400

    Add dynamic alerts

--- ircbeta/src/screens/AlertScreen.js
+++ ircbeta/src/screens/AlertScreen.js
@@ -3,53 +3,36 @@ import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { connect } from 'react-redux';
 import { database } from '../firebase/firebase';
 import Panel from '../components/Panel';
-import firebase from 'firebase'
 
 class AlertScreen extends Component {
-  state = {
-    alerts: [
-      {
-        title: 'Hello',
-        description: "I'm dead",
-        tier: 7
-      },
-      {
-        title: 'Hellagagago',
-        description: "I'm deafad",
-        tier: 2
-      },
-      {
-        title: 'Hellagagagago',
-        description: "I'm deafafafad",
-        tier: 4
-      }
-    ]
-  }
+    constructor(props) {
+        super(props);
+        this.state = {};
+    };
+    componentWillMount() {
+        console.log('Componeontwm')
+        const alerts = database.ref('/Syria/alerts/');
+        alerts.on('value', (snapshot) => {
+            console.log(snapshot.val());
+            this.setState({
+                alerts: snapshot.val(),
+            });
+        });
+    };
   componentDidMount() {
-    // database.ref('/staff').once('value').then((snapshot) => {
-    //   console.log(snapshot)
-    // })
-    console.log(this.state.alerts)
-    database.ref('/staff').once('value').then((snapshot) => {
-      console.log(snapshot)
-    })
-  }
-
-  componenetWillMount() {
-     this.props.alerts;
-  }
-
+  };
   render() {
     return(
       <View style={{ flex: 1 }}>
-      {this.state.alerts.map((listObject, index) => {
-        console.log(listObject)
+      {this.state.alerts &&
+          this.state.alerts.map((alertItem, index) => {
         return (
-        <Panel
-          title={listObject.title}
-          description={listObject.description}
-          key={index}
-        />
+            <Panel
+              title={alertItem.title}
+              description={alertItem.description}
+              tier={alertItem.tier}
+              key={index}
+            />
         )
       })}
       </View>
@@ -70,11 +53,18 @@ const styles = StyleSheet.create({
 
 const mapStateToProps = (state, ownProps) => {
   console.log('AlertScreen state', state)
-  const alertList = state.alert.alerts;
+  // const alertList = state.alert.alerts;
   return {
     ...ownProps,
-    alerts: alertList,
+    // alerts: alertList,
   };
 }
 
-export default connect(null)(AlertScreen);
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    ...ownProps,
+    dispatch: (action) => dispatch(action),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(AlertScreen);
