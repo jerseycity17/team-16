fcb42cb646b6f356f2015282eb1d598f72ad3369
Alex Supkay <asupkay1124@gmail.com>
2017-10-27 22:34:00 -0400

    Add styles to panel

--- ircbeta/src/components/Panel.js
+++ ircbeta/src/components/Panel.js
@@ -51,4 +51,33 @@ class Panel extends Component{
         );
     }
 }
+
+var styles = StyleSheet.create({
+    container   : {
+        backgroundColor: '#fff',
+        margin:10,
+        overflow:'hidden'
+    },
+    titleContainer : {
+        flexDirection: 'row'
+    },
+    title       : {
+        flex    : 1,
+        padding : 10,
+        color   :'#2a2f43',
+        fontWeight:'bold'
+    },
+    button      : {
+
+    },
+    buttonImage : {
+        width   : 30,
+        height  : 25
+    },
+    body        : {
+        padding     : 10,
+        paddingTop  : 0
+    }
+});
+
 export default Panel;
