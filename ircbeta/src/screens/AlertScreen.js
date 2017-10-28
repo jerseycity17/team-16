f33410d98d1809936dde2602d52d157d466bbae0
Matt <mattcolozzo@gmail.com>
2017-10-27 23:34:23 -0400

    added the store import for alerts

--- ircbeta/src/screens/AlertScreen.js
+++ ircbeta/src/screens/AlertScreen.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { connect } from 'react-redux';
 import { database } from '../firebase/firebase';
+import store from '../store/configureStore';
 import firebase from 'firebase'
 
 class AlertScreen extends Component {
